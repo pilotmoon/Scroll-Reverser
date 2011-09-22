5ac45b54f2326ed70779ddd4807e0abcb1fb9843
Nick Moore <nick@pilotmoon.com>
2011-09-22 16:55:25 +0100

    deleted loads of stuff (in a mess)

--- DCActivation.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-@protocol Activation
-
-@property (assign, getter=isActive) BOOL active;
-- (void)start;
-- (void)stop;
-
-@end
--- NSImage+CopySize.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface NSImage(CopySize) 
-
-- (NSImage *)copyWithSize:(NSSize)size colorTo:(NSColor *)color;
-- (NSImage *)copyWithSize:(NSSize)size;
-+ (NSImage *)newBlankBitmapOfSize:(NSSize)size;
-
-@end
