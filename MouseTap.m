c918427d0ba5d47e9171fc03e0fade77cd9795e8
Nick Moore <nick@pilotmoon.com>
2013-03-25 20:57:43 +0000

    add comments to mousetap

--- MouseTap.m
+++ MouseTap.m
@@ -23,7 +23,7 @@ static NSString *_bundleIdForPID(const pid_t pid)
 }
 
 /*
- Is the pid a wacom tablet?
+ Is the pid a wacom tablet? Crude method.
  */
 static BOOL _pidIsWacomTablet(const pid_t pid)
 {
@@ -47,27 +47,26 @@ static BOOL _pidIsWacomTablet(const pid_t pid)
 }
 
 /* 
- Work out the scrolling phase
+ We abstract the system defined scrolling phases into these possibilities.
  */
 typedef enum {
-    ScrollPhaseNormal=0,
-    ScrollPhaseMomentum,
-    ScrollPhaseEnd
+    ScrollPhaseNormal=0, // fingers on pad
+    ScrollPhaseMomentum, // fingers off pad, but scrolling with momentum
+    ScrollPhaseEnd       // scrolling ended
 } ScrollPhase;
 
 static ScrollPhase _momentumPhaseForEvent(CGEventRef event)
 {
     ScrollPhase result=ScrollPhaseNormal;
-
+    
     const NSEventPhase momentumPhase=[[NSEvent eventWithCGEvent:event] momentumPhase];
     
-    if (momentumPhase==4) {
+    if (momentumPhase==NSTouchPhaseStationary) {
         result=ScrollPhaseMomentum;
     }
-    else if (momentumPhase==8) {
+    else if (momentumPhase==NSTouchPhaseEnded) {
         result=ScrollPhaseEnd;
     }
-
     return result;
 }
 
@@ -97,108 +96,116 @@ static void _doReversal(MouseTap *tap, CGEventRef event)
 
 // This is called every time there is a scroll event. It has to be efficient.
 static CGEventRef eventTapCallback(CGEventTapProxy proxy,
-							 CGEventType type,
-							 CGEventRef event,
-							 void *userInfo)
-{    
+                                   CGEventType type,
+                                   CGEventRef event,
+                                   void *userInfo)
+{
     @autoreleasepool {
 		MouseTap *tap=(__bridge MouseTap *)userInfo;
-    
-    if (type==NSEventTypeGesture)
-    {
-        // how many fingers on the pad
-        NSEvent *ev=[NSEvent eventWithCGEvent:event];
-        tap->fingers=[[ev touchesMatchingPhase:NSTouchPhaseTouching inView:nil] count];		
-        //NSLog(@"fingers %lu", tap->fingers);
-    }
-    else if (type==NSScrollWheel)
-    {
-        // determine source
-        ScrollEventSource source=ScrollEventSourceOther;
-
-        // check if tablet
-        const uint64_t pid=CGEventGetIntegerValueField(event, kCGEventSourceUnixProcessID);
-        if (pid&&_pidIsWacomTablet(pid))
-        {
-            source=ScrollEventSourceTablet;
-        }
-        else
-        {
-            const ScrollPhase phase=_momentumPhaseForEvent(event);
-            const UInt32 ticks=TickCount(); // about 1/60 of a sec
-            const UInt32 ticksElapsed=ticks-tap->lastScrollTicks;
-
-            //NSLog(@"scroll %i", phase);
-            
-            if (phase==ScrollPhaseNormal&&(tap->lastPhase!=ScrollPhaseNormal||tap->sampledFingers<_minFingers||tap->zeroCount>_minZeros||ticksElapsed>20))
-            {
-                tap->sampledFingers=tap->fingers;
-                //NSLog(@"Sampled %lu fingers", tap->sampledFingers);
-            }
-             
-            if (tap->fingers>=_minFingers) {
-                tap->zeroCount=0;
-            }
-            else {
-                tap->zeroCount+=1;
-            }
-
-            tap->lastPhase=phase;     
-            tap->lastScrollTicks=TickCount();
-            
-            if (tap->sampledFingers>=_minFingers)
-            {
-                source=ScrollEventSourceTrackpad;
-            }
-        }
         
-        //NSLog(@"source %i", source);        
+        if (type==NSEventTypeGesture)
+        {
+            // how many fingers on the trackpad?
+            NSEvent *ev=[NSEvent eventWithCGEvent:event];
+            tap->fingers=[[ev touchesMatchingPhase:NSTouchPhaseTouching inView:nil] count];
+            //NSLog(@"fingers %lu", tap->fingers);
+        }
+        else if (type==NSScrollWheel)
+        {
+            // determine source
+            ScrollEventSource source=ScrollEventSourceOther;
+            
+            // check if tablet
+            const uint64_t pid=CGEventGetIntegerValueField(event, kCGEventSourceUnixProcessID);
+            if (pid&&_pidIsWacomTablet(pid))
+            {
+                source=ScrollEventSourceTablet;
+            }
+            else
+            {
+                const ScrollPhase phase=_momentumPhaseForEvent(event);
+                const UInt32 ticks=TickCount(); // about 1/60 of a sec
+                const UInt32 ticksElapsed=ticks-tap->lastScrollTicks;
                 
-        // don't reverse scrolling we have already reversed
-        const int64_t ud=CGEventGetIntegerValueField(event, kCGEventSourceUserData);
-        const BOOL preventBecauseOfMagicNumber=ud==MAGIC_NUMBER;
-        
-        // don't reverse scrolling which comes from another app (if that setting is on)
-        BOOL preventBecauseComingFromOtherApp=NO;
-        if(_preventReverseOtherApp)
-        {
-            int64_t sourcepid=CGEventGetIntegerValueField(event, kCGEventSourceUnixProcessID);				
-            if (sourcepid!=0)
+                //NSLog(@"scroll %i", phase);
+                
+                // Should we sample the number of fingers now?
+                // The whole point of this is to only sample fingers when user is actually scrolling, not during the momentum phase.
+                // Unfortunately the system cannot be relied upon to alwayts send correct finger signals (four finger swipes for example
+                // will mess things up) so we use some timing and other indicators. Still room for improvement here.
+                if (phase==ScrollPhaseNormal&&(tap->lastPhase!=ScrollPhaseNormal||tap->sampledFingers<_minFingers||tap->zeroCount>_minZeros||ticksElapsed>20))
+                {
+                    tap->sampledFingers=tap->fingers;
+                    //NSLog(@"Sampled %lu fingers", tap->sampledFingers);
+                }
+                
+                // Count of how many times we have seen no fingers on the pad.
+                if (tap->fingers>=_minFingers) {
+                    tap->zeroCount=0;
+                }
+                else {
+                    tap->zeroCount+=1;
+                }
+                
+                tap->lastPhase=phase;
+                tap->lastScrollTicks=TickCount();
+                
+                // Assume Trackpad source when the required number of fingers is seen on the pad.
+                if (tap->sampledFingers>=_minFingers)
+                {
+                    source=ScrollEventSourceTrackpad;
+                }
+            }
+            
+            //NSLog(@"source %i", source);
+            
+            // don't reverse scrolling we have already reversed
+            const int64_t ud=CGEventGetIntegerValueField(event, kCGEventSourceUserData);
+            const BOOL preventBecauseOfMagicNumber=ud==MAGIC_NUMBER;
+            
+            // don't reverse scrolling which comes from another app (if that setting is on)
+            // this is useful Scroll Reverser running inside a remote desktop, to ignore scrolling coming from
+            // the controlling host but still reverse local scrolling.
+            BOOL preventBecauseComingFromOtherApp=NO;
+            if(_preventReverseOtherApp)
             {
-                preventBecauseComingFromOtherApp=YES;
+                int64_t sourcepid=CGEventGetIntegerValueField(event, kCGEventSourceUnixProcessID);
+                if (sourcepid!=0)
+                {
+                    preventBecauseComingFromOtherApp=YES;
+                }
+            }
+            
+            if (tap->inverting&&!(preventBecauseOfMagicNumber||preventBecauseComingFromOtherApp))
+            {
+                BOOL invert=YES;
+                switch (source)
+                {
+                    case ScrollEventSourceTrackpad:
+                        invert=tap->invertMultiTouch;
+                        break;
+                        
+                    case ScrollEventSourceTablet:
+                        invert=tap->invertTablet;
+                        break;
+                        
+                    case ScrollEventSourceOther:
+                    default:
+                        invert=tap->invertOther;
+                        break;
+                }
+                if (invert)
+                {
+                    _doReversal(tap, event);
+                }
             }
         }
+        else if(type==kCGEventTapDisabledByTimeout)
+        { 
+            // This can happen sometimes. (Not sure why.) 
+            [tap enableTap:TRUE]; // Just re-enable it.
+        }	
         
-        if (tap->inverting&&!(preventBecauseOfMagicNumber||preventBecauseComingFromOtherApp))
-        {
-            BOOL invert=YES;
-            switch (source)
-            {
-                case ScrollEventSourceTrackpad:
-                    invert=tap->invertMultiTouch;
-                    break;
-                    
-                case ScrollEventSourceTablet:
-                    invert=tap->invertTablet;
-                    break;
-                    
-                case ScrollEventSourceOther:
-                default:
-                    invert=tap->invertOther;
-                    break;
-            }
-            if (invert)
-            {
-                _doReversal(tap, event);
-            }
-        }
-    }
-    else if(type==kCGEventTapDisabledByTimeout)
-    { 
-        // This can happen sometimes. (Not sure why.) 
-        [tap enableTap:TRUE]; // Just re-enable it.
-    }	
-    
 		return event;
 	}
 }
