19911367c7fec54bc9755871824d72f1dc7f5bef
Nick Moore <nick@pilotmoon.com>
2014-09-25 20:07:44 +0100

    Update build scripts to my latest versions

--- BuildScripts/insert_version.sh
+++ BuildScripts/insert_version.sh
@@ -1,27 +1,58 @@
 # This script fixes up the CFBundleShortVersionString with a string derived from git.
 # Place it as a Build Phase just before Copy Bundle Resources
 
-# clone: git submodule add git@gist.github.com:1151287.git gist-1151287
-# call:  ${SRCROOT}/gist-1151287/insert_version.sh
+if [[ "$1" ]]; then
+	clean=$1
+else 
+	clean='noclean'
+fi
+
+echo "clean setting: $clean"
+
+if [[ "$2" ]]; then
+	buildnum_offset=$2
+else 
+	buildnum_offset='0'
+fi
+
+echo "build number offset: $buildnum_offset"
 
 # PlistBuddy and git executables
 buddy='/usr/libexec/PlistBuddy'
 git='/usr/bin/git'
 
-# the plist file and key to replace
-plist=${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}
+# the plist files and keys to replace
+plist="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"
+sym_plist="${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}.dSYM/Contents/Info.plist"
+
 key='CFBundleShortVersionString'
+num_key='CFBundleVersion'
+record_key='NMOriginalBundleShortVersionString'
+
+#build number (needs first commit to be tagged 'buildbase')
+buildbase_string=`$git describe --match buildbase`
+buildnum=`echo "$buildbase_string" | sed 's/buildbase\-//' | sed 's/-g.*//'`
+echo "build number from git is $buildnum"
+buildnum=`expr $buildnum + $buildnum_offset`
+echo "adjusted build number is $buildnum"
 
 # version string
-version=`$git describe --dirty`
-if [ ${CONFIGURATION} == 'Debug' ]
-then
+version=`$git describe --dirty --tags`
+echo "build number from git is $version"
+
+# add debug suffix if debug
+if [[ "${CONFIGURATION}" == 'Debug' ]]; then
+	echo "it is a debug build, appending -d"
     version="$version-d"
 fi
 
-# clean string if Release build
-if [ $1 == 'clean' ]
-then
+# add the key to record the original version string
+echo "Setting $record_key to $version in $plist"
+$buddy -c "Delete :$record_key" "$plist"
+$buddy -c "Add :$record_key string $version" "$plist"
+
+# clean string if indicated
+if [[ "$clean" == "clean" ]]; then
   # version string for release builds  (strip off everything after dash, e.g. 1.0.2)
   # i do this so that i can test appstore submission on builds tagged e.g. 1.0.2-test1 
   clean_version=`echo $version | sed 's/\-.*//'`
@@ -30,5 +61,15 @@ then
 fi
 
 # do the replacement
-echo "Setting $key to $version in Info.plist"
+echo "Setting $key to $version in $plist"
 $buddy -c "Set :$key $version" "$plist"
+
+echo "Setting $key to $version in $sym_plist"
+$buddy -c "Set :$key $version" "$sym_plist"
+
+echo "Setting $num_key to $buildnum in $plist"
+$buddy -c "Set :$num_key $buildnum" "$plist"
+
+echo "Setting $num_key to $buildnum in $sym_plist"
+$buddy -c "Set :$num_key $buildnum" "$sym_plist"
+
---
+++ ReleaseNotes.md
@@ -0,0 +1,69 @@
+### v1.6.1 (TBA)
+
+* Updated for OS X Yosemite.
+
+### v1.6 (21 May 2013)
+
+* Updated the status icon for retina displays.
+* Better accuracy detecting traditional wheel mice from trackpads. (No change to Magic and Mighty mouse detection.)
+* Added the option to automatically check for updates.
+* Fix minor bug which casused a console warning about 'scrollPhase is deprecated...'.
+* Requires OS X 10.7 Lion and above. (Version 1.5.1 still supports 10.4 and above.)
+
+### v1.5.1 (12 March 2012)
+
+* Signed with a Developer ID for OS X 10.8 Gatekeeper.
+
+### v1.5 (23 February 2012)
+
+* Now works with OS X 10.4 Tiger.
+* More reliable detection between mouse and trackpad.
+* Fixed a bug that led to high memory usage.
+* Added a welcome window that appears on the first run.
+* Added German localization. (Thanks, Peter.)
+
+### v1.4.4 (29 September 2011)
+
+* Fixed a memory leak.
+* Fixed a problem with the Italian localization.
+* Added Romanian localization. (Thanks, Gabriel.)
+
+### v1.4.3 (9 September 2011)
+
+* Added French localization. (Thanks, Marcel and E.)
+
+### v1.4.2 (18 August 2011)
+
+* Changed default on Lion to not reverse horizontal scrolling.
+* Added Spanish localization. (Thanks, Pedro.)
+
+### v1.4.1 (18 August 2011)
+
+* Restored PowerPC and 32-bit Intel support.
+* Better Wacom tablet detection.
+
+### v1.4 (11 August 2011)
+
+* Added independent settings for horizontal and vertical scrolling.
+* Added independent settings for scrolling with trackpad, mouse and tablet (e.g. Wacom).
+* Fixed a Firefox compatibility problem.
+* Dropped PowerPC and Intel 32-bit support (by mistake).
+* Added Italian localization. (Thanks, Vincenzo.)
+
+### v1.3 (28 June 2011)
+
+* Added PowerPC support.
+* Added Greek localization. (Thanks, ecrisis.)
+
+### v1.2 (11 April 2011)
+
+* Added option to hide the menu bar icon.
+* Added Brazilian Portuguese localization. (Thanks, Humberto.)
+
+### v1.1 (1 April 2011)
+
+* Bugfixes.
+
+### v1.0 (8 March 2011)
+
+* Initial release.
